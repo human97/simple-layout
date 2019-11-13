890400b2f0cc324649848b02f13f466beea4de28
human97 <aleksey.n.popov@gmail.com>
2019-11-13 21:37:02 +0400

    revert#01

--- js/slick.js
+++ js/slick.js
@@ -1,14 +1,16 @@
 $('.slider1').slick({
     autoplay: true,
     autoplaySpeed: 1000,
-    speed:2000,
+    speed: 2000,
 });
 
-menu.onclick = function myFunction(){
-var x = document.getElementById('myTopnav');
-if(x.className === 'boxTopnav'){
-x.className += ' responsive';
-}else{
-x.className = 'boxTopnav';
-}
+let menu = document.getElementById('menu')
+menu.onclick = function myFunction() {
+    let x = document.getElementById('myTopnav')
+    x.classList.toggle('responsive')
+    /*if (x.className === 'boxTopnav') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'boxTopnav';
+    }*/
 }
